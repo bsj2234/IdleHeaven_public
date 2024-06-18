a2030ff117e89db1851ddf4007bb7259f9c60d0c
변석진 <bsj2234@naver.com>
2024-06-18 20:18:53 +0900

    [update] itemgenerator 멤버 추

--- Assets/Scripts/Inventory/ItemSpawner.cs
+++ Assets/Scripts/Inventory/ItemSpawner.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using IdleHeaven;
 
 public class ItemSpawner : MonoBehaviour
 {
+    [SerializeField] public ItemGenerator generator;
     public DroppedItem SpawnItem(Transform position, GameObject itemPrefab)
     {
         Instantiate(itemPrefab, position.position, position.rotation)
