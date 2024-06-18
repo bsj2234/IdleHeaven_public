0dee5fa59854c2049d0a5540b2cc348990a25814
변석진 <bsj2234@naver.com>
2024-06-18 13:48:46 +0900

    [update] 코드 정리

--- Assets/Scripts/AICharacterController.cs
+++ Assets/Scripts/AICharacterController.cs
@@ -15,21 +15,20 @@ public interface IMovableAI: IMovable
 [RequireComponent (typeof(StateMachine))]
 public class AICharacterController : MonoBehaviour, IMovableAI, ICombat
 {
+
+    private Combat combat = new Combat();
+    private Weapon weapon;
+
+    [SerializeField] private NavMeshAgent Agent_movement;
+
     private StateMachine stateMachine;
     public Transform[] patrolWaypoints;
     public Transform chaseTarget;
-    [SerializeField] private NavMeshAgent Agent_agent;
-    public IMovable movement;
-    private Weapon weapon;
-
-    private Combat combat = new Combat();
-
     public event Action<Transform> EnemyFoundHandler;
-
     private List<ICombat> attackAbleEnemies = new List<ICombat>();
 
-    //IsGrounded(Walk,Run)	IsFlying	IsSwimming	IsClimbing	IsJumping IsBurrowing	IsSliding	IsSwinging	IsCreeping	IsRolling	IsFloating	IsGliding	IsSoaring
 
+    //IsGrounded(Walk,Run)	IsFlying	IsSwimming	IsClimbing	IsJumping IsBurrowing	IsSliding	IsSwinging	IsCreeping	IsRolling	IsFloating	IsGliding	IsSoaring
     private void Start()
     {
         combat.Init(transform);
@@ -51,23 +50,10 @@ public class AICharacterController : MonoBehaviour, IMovableAI, ICombat
 
         stateMachine.ChangeState<IdleState>();
     }
-
-
-    public NavMeshAgent GetAgent()
-    {
-        return Agent_agent;
-    }
-
-    public Combat GetCombat()
-    {
-        return combat;
-    }
-
     public void ChangeToAuto()
     {
         stateMachine.ChangeState<AttackState>();
     }
-
     private void OnFoundEnemy(ICombat target)
     {
 
@@ -76,44 +62,19 @@ public class AICharacterController : MonoBehaviour, IMovableAI, ICombat
 
         if (stateMachine.CurrentState != stateMachine.GetState<AttackState>())
         {
-            stateMachine.GetState<ChaseState>().SetTarget(PeekNearestEnemy().GetTransform());
+            stateMachine.GetState<ChaseState>().SetTarget(GetNearestEnemy().GetTransform());
             stateMachine.ChangeState<ChaseState>();
         }
 
         if (EnemyFoundHandler != null)
         {
-            EnemyFoundHandler.Invoke(PeekNearestEnemy().GetTransform());
+            EnemyFoundHandler.Invoke(GetNearestEnemy().GetTransform());
         }
         else
         {
             Debug.Log("Enemy found but handler is null");
         }
     }
-
-    public ICombat PeekNearestEnemy()
-    {
-        if(attackAbleEnemies.Count == 0)
-        {
-            return null;
-        }
-        attackAbleEnemies.Sort((lhs, rhs) => (rhs.GetDistance(transform.position).CompareTo(lhs.GetDistance(transform.position))));
-        var result = attackAbleEnemies[attackAbleEnemies.Count - 1];
-        //attackAbleEnemies.RemoveAt(attackAbleEnemies.Count - 1);
-        Debug.Log($"Pop AttackableEnemy {result.GetTransform().name}");
-        return result;
-    }
-
-    public void PopEnemy(ICombat combat)
-    {
-        if (attackAbleEnemies.Count == 0)
-        {
-            Debug.Log("Tried pop but empty");
-            return;
-        }
-        attackAbleEnemies.Remove(combat);
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         //Debug.Log($"{name} Trigger Enter");
@@ -122,46 +83,121 @@ public class AICharacterController : MonoBehaviour, IMovableAI, ICombat
             OnFoundEnemy(other.transform.GetComponent<ICombat>());
         }
     }
-
-    public void DealDamage(ICombat enemyCombat, float damage)
+    private void OnTriggerExit(Collider other)
     {
-        enemyCombat.TakeDamage(combat, damage);
+        if(other.CompareTag("Enemy"))
+        {
+            if(TryGetComponent(out ICombat enemyCombat))
+            {
+                if(!combat.IsDead())
+                {
+                    OnLooseEnemy(enemyCombat);
+                }
+            }
+        }
     }
-    public void TakeDamage(ICombat enemyCombat, float damage)
+    private void OnLooseEnemy(ICombat enemyCombat)
+    {
+        attackAbleEnemies.Remove(enemyCombat);
+    }
+    public ICombat GetNearestEnemy()
+    {
+        if (attackAbleEnemies.Count == 0)
+        {
+            return null;
+        }
+        attackAbleEnemies.Sort(SortEnemy);
+        attackAbleEnemies.RemoveAll((item) => item.GetGameObject() == null);
+        string Enemy_array = "";
+        foreach(var enemy in attackAbleEnemies)
+        {
+            Enemy_array = $"{Enemy_array} ,{enemy.GetTransform().name}";
+        }
+        Debug.Log(Enemy_array);
+
+        int i = attackAbleEnemies.Count - 1;
+        var result = attackAbleEnemies[i];
+        while(result.IsDead())
+        {
+            attackAbleEnemies.RemoveAt(i--);
+            result = attackAbleEnemies[i];
+        }
+        return result;
+    }
+
+    private int SortEnemy(ICombat lhs, ICombat rhs)
+    {
+        if(lhs == null)
+        {
+            Debug.Log(1);
+            return -1;
+        }
+        if(rhs == null)
+        {
+            Debug.Log(2);
+            return 1;
+        }
+        if(lhs.GetGameObject() == null)
+        {
+            Debug.Log(3);
+
+            return -1;
+        }
+        if( rhs.GetGameObject() == null)
+        {
+            Debug.Log(4);
+            return 1;
+        }
+        return rhs.GetDistance(transform.position).CompareTo(lhs.GetDistance(transform.position));
+    }
+
+    public void RemoveEnemy(ICombat combat)
+    {
+        if (attackAbleEnemies.Count == 0)
+        {
+            Debug.Log("Tried pop but empty");
+            return;
+        }
+        attackAbleEnemies.Remove(combat);
+    }
+    public NavMeshAgent GetAgent()
+    {
+        return Agent_movement;
+    }
+    public Combat GetCombat()
+    {
+        return combat;
+    }
+    public void TakeDamage(ICombat attackerCombat, float damage)
     {
         combat.TakeDamage(damage);
     }
-
-    private void OnAttack()
+    public void Attack(ICombat targetCombat, float damage)
     {
-        weapon.OnCollide();
+        combat.DealDamage( targetCombat.GetCombat(),damage);
     }
-    private void OnTakeDamage()
-    {
-    }
-
-    public void TakeDamage(Combat attackerCombat, float damage)
-    {
-        combat.TakeDamage(damage);
-    }
-
-    public void Attack(Combat targetCombat, float damage)
-    {
-        combat.DealDamage( targetCombat,damage);
-    }
-
     public bool IsDead()
     {
         return combat.IsDead();
     }
-
     public float GetDistance(Vector3 origin)
     {
         return Vector3.Distance(origin, transform.position);
     }
-
     public Transform GetTransform()
     {
         return transform;
+    }
+
+    public void DestryRandomNearEnemy()
+    {
+        Destroy(GetNearestEnemy().GetTransform().gameObject);
+    }
+
+    public GameObject GetGameObject()
+    {
+        if (this == null)
+            return null;
+        return gameObject;
     }
 }
