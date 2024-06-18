867f4791abbca350f22329e4a746229068997ffc
변석진 <bsj2234@naver.com>
2024-06-18 15:25:39 +0900

    [Update] 부드럽게 획득 하도록 하려다 slerp 말고 cos 을 사용

--- Assets/Scripts/Inventory/ItemGrabber.cs
+++ Assets/Scripts/Inventory/ItemGrabber.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ItemGrabber : MonoBehaviour
 {
     [SerializeField] Transform _target;
+    [SerializeField] float _height;
+    [SerializeField] float _duration;
     private Vector3 _initialPos;
     private float _time;
-    [SerializeField]private float _duration;
 
-    private void Grab()
+    public void Grab()
     {
+        _target = FindAnyObjectByType<AICharacterController>().transform;
         _initialPos = transform.position;
     }
 
@@ -19,13 +21,14 @@ public class ItemGrabber : MonoBehaviour
         Debug.Assert(_target != null);
         //처음 위치에서 타겟 위치까지 이동시킴
         Vector3 tempPos = Vector3.Lerp(_initialPos, _target.position, _time / _duration);
-        //처음엔 높이 0 이었다가 중간에 1 타겟위치에서는 0
-        // 0 ~ 1  -.5f abs
+        float nomyTime = _time / _duration;
+        float halfOfDuration = .5f;
+        float yTime = nomyTime - halfOfDuration;
+        yTime = Mathf.Min(yTime, halfOfDuration);
+        yTime = Mathf.Cos(Mathf.PI * -yTime);
+        Vector3 yOffset = Vector3.up * yTime * _height;
 
-        float yTime = Mathf.Abs(_time - _duration * .5f);
-        Vector3 yOffset = Vector3.Slerp(Vector3.zero, Vector3.up * 5f, yTime);
-
-        _target.position = tempPos;
+        transform.position = tempPos + yOffset;
 
         _time += Time.fixedDeltaTime;
     }
