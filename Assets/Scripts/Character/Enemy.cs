ff14a72f00aaf0cf79352606a1cdb697662129bb
변석진 <bsj2234@naver.com>
2024-06-19 14:25:11 +0900

    [rename] 폴더 정리

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Attack))]
-public class Enemy1 : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     [SerializeField] bool autoSet = false;
     [SerializeField] Health combat;
