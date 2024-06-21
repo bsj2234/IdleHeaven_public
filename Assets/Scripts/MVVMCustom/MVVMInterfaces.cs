bd3a4fc9623907ba32b5c910fb994a935ab29551
변석진 <bsj2234@naver.com>
2024-06-21 22:33:00 +0900

    [update] mvvm 미사용 부분 삭제

--- Assets/Scripts/MVVMCustom/MVVMInterfaces.cs
+++ Assets/Scripts/MVVMCustom/MVVMInterfaces.cs
@@ -12,18 +12,4 @@ namespace IdleHeaven
         void Initialize(IViewModel viewModel);
         void Refresh();
     }
-
-
-    public class Notifier : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-    }
 }
