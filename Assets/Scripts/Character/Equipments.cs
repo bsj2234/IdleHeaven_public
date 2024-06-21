79f184dba8f3a286f5ab959d240de0a08146f824
변석진 <bsj2234@naver.com>
2024-06-21 11:46:15 +0900

    [update] 장착시 이벤트 발생 추가

--- Assets/Scripts/Character/Equipments.cs
+++ Assets/Scripts/Character/Equipments.cs
@@ -1,29 +1,73 @@
-using IdleHeaven;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield }
-
-public class Equipments : MonoBehaviour
+namespace IdleHeaven
 {
-    public EquipmentItem[] slot;
-    public CharacterStats statBonuses;
+    public enum EquipmentSlot { Head, Chest, Legs, Weapon, Shield }
 
-    public Action<EquipmentItem> OnEquipped;
-    public Action<EquipmentItem> OnUnEquipped;
-
-    private void Awake()
+    public class Equipments : MonoBehaviour
     {
-        slot = new EquipmentItem[System.Enum.GetValues(typeof(EquipmentSlot)).Length;
-    }
+        [SerializeField] private EquipmentItem[] _slotVisualize;
+        private Dictionary<EquipmentSlot, EquipmentItem> _equippedItems = new Dictionary<EquipmentSlot, EquipmentItem>();
 
-    public void Equip(EquipmentItem item)
-    {
-        OnEquipped.Invoke(item);
-    }
+        public event Action<Equipments, EquipmentSlot, EquipmentItem> OnEquipped;
+        public event Action<Equipments, EquipmentSlot, EquipmentItem> OnUnEquipped;
 
-    public void Unequip(EquipmentItem item)
-    {
-        OnUnEquipped.Invoke(item);
+        public EquipmentItem Equip(EquipmentSlot slot, EquipmentItem item)
+        {
+            EquipmentItem previousItem = null;
+            if (_equippedItems.ContainsKey(slot))
+            {
+                previousItem = _equippedItems[slot];
+                _equippedItems[slot] = item;
+            }
+            else
+            {
+                _equippedItems.Add(slot, item);
+            }
+
+            OnEquipped?.Invoke(this, slot, item);
+            return previousItem;
+        }
+
+        public EquipmentItem Unequip(EquipmentSlot slot)
+        {
+            EquipmentItem previousItem = null;
+            if (_equippedItems.ContainsKey(slot))
+            {
+                previousItem = _equippedItems[slot];
+                _equippedItems.Remove(slot);
+                OnUnEquipped?.Invoke(this, slot, previousItem);
+            }
+            return previousItem;
+        }
+
+        public EquipmentItem GetEquippedItem(EquipmentSlot slot)
+        {
+            _equippedItems.TryGetValue(slot, out var item);
+            return item;
+        }
+
+        public Dictionary<EquipmentSlot, EquipmentItem> GetEquippedItems()
+        {
+            return new Dictionary<EquipmentSlot, EquipmentItem>(_equippedItems);
+        }
+
+        public bool CanEquip(EquipmentSlot slot, EquipmentItem item)
+        {
+            // Add your own validation logic here
+            return true;
+        }
+
+        public void ClearAllEquipment()
+        {
+            var equippedItemsCopy = new Dictionary<EquipmentSlot, EquipmentItem>(_equippedItems);
+            _equippedItems.Clear();
+            foreach (var kvp in equippedItemsCopy)
+            {
+                OnUnEquipped?.Invoke(this, kvp.Key, kvp.Value);
+            }
+        }
     }
 }
