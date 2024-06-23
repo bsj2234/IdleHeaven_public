fd437726fabf32a374940c06195d2c963a28dc4b
변석진 <bsj2234@naver.com>
2024-06-24 03:36:58 +0900

    [update] 게임매니저 코드 정리

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,8 @@
 using UnityEngine;
 
-public class GameManager: MonoBehaviour
+public class GameManager: MonoSingleton<GameManager>
 {
-    private static GameManager _instance;
     [SerializeField] Player _player;
-
-    public static GameManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                Debug.Assert(false, "NO GameManager In Scene");
-            }
-            return _instance;
-        }
-    }
 
     public Player GetPlayer()
     {
