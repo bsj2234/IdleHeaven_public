00c3bff619bf51dc3bbab0a45fd2836a90dffe90
변석진 <bsj2234@naver.com>
2024-07-01 10:42:38 +0900

    [rename] 오탈자 수정

--- Assets/Scripts/UI/HorizontalNamedLable.cs
+++ Assets/Scripts/UI/HorizontalNamedLable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HrizontalNamedLable : MonoBehaviour
+public class HorizontalNamedLable : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI Name;
     public TMPro.TextMeshProUGUI Value;
