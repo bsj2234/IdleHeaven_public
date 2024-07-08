637a7fe32dafefa17135a0c6b9d859161fbd06f9
변석진 <bsj2234@naver.com>
2024-07-09 01:01:31 +0900

    [update] 레어리티에 재화, 없음 추가 사실 여기는 아닌것 같은데

--- Assets/IdleHeaven/Scripts/Effect/ItemEffectData.cs
+++ Assets/IdleHeaven/Scripts/Effect/ItemEffectData.cs
@@ -5,6 +5,8 @@ namespace IdleHeaven
     [Serializable]
     public enum Rarity
     {
+        None,
+        Currency,
         Common,
         Uncommon,
         Rare,
@@ -14,48 +16,6 @@ namespace IdleHeaven
     }
 
 
-    public class RarityData
-    {
-        private static readonly RarityData[] rarityData = new RarityData[]
-        {
-        new RarityData(Rarity.Common, "Common", Color.gray, 1f, 1f),
-        new RarityData(Rarity.Uncommon, "Uncommon", Color.green, 1f, 1.1f),
-        new RarityData(Rarity.Rare, "Rare", Color.blue, 1.1f, 1.5f),
-        new RarityData(Rarity.Epic, "Epic", Color.magenta, 1.5f, 2f),
-        new RarityData(Rarity.Legendary, "Legendary", Color.yellow, 2f, 3f)
-        };
-
-        public Rarity Rarity { get; private set; }
-        public string Name { get; private set; }
-        public Color Color { get; private set; }
-
-        public float MinBaseStatMulti { get; private set; }
-        public float MaxBaseStatMulti { get; private set; }
-
-        private RarityData(Rarity rarity, string name, Color color, float minBaseStatMulti, float maxBaseStatMulti)
-        {
-            Rarity = rarity;
-            Name = name;
-            Color = color;
-            MinBaseStatMulti = minBaseStatMulti;
-            MaxBaseStatMulti = maxBaseStatMulti;
-        }
-
-        public static RarityData GetRarityData(Rarity rarity)
-        {
-            return rarityData[(int)rarity];
-        }
-
-        public static string GetRarityName(Rarity rarity)
-        {
-            return GetRarityData(rarity).Name;
-        }
-
-        public static Color GetRarityColor(Rarity rarity)
-        {
-            return GetRarityData(rarity).Color;
-        }
-    }
 
 
 
