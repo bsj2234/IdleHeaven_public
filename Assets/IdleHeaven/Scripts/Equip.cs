9bdb42b0936cbb6bce0a7ae465465c0e27e75b74
변석진 <bsj2234@naver.com>
2024-07-09 01:12:36 +0900

    [update] 장착을 매개변수로, 성공실패 여부 반환

--- Assets/IdleHeaven/Scripts/Equip.cs
+++ Assets/IdleHeaven/Scripts/Equip.cs
@@ -7,16 +7,18 @@ public class Equip : MonoBehaviour
 {
     [SerializeField] Equipments _equipments;
     [SerializeField] ItemView _itemView;
-    public void EquipItem()
+    public bool EquipItem(Item item)
     {
-        EquipmentItem equipment = _itemView.ItemViewModel.Item as EquipmentItem;
+        EquipmentItem equipment = item as EquipmentItem;
         if(equipment != null)
         {
             _equipments.Equip(equipment);
+            return true;
         }    
-        else 
+        else
         {
             Debug.Log("Item is not equipment");
+            return false;
         }
     }
 }
