322f07c27fdebf62048ebcf3bb77d0573c4d86b4
변석진 <bsj2234@naver.com>
2024-07-09 00:55:19 +0900

    [update] 아이템 레어도 데이터 스크림터블 오브젝트 추가

---
+++ Assets/IdleHeaven/Scripts/ScriptableObject/RarityImageData.cs
@@ -0,0 +1,14 @@
+using IdleHeaven;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RarityData", menuName = "IdleHeaven/RarityData", order = 0)]
+
+public class RarityData : ScriptableObject
+{
+    [field: SerializeField] public Rarity Rarity { get; private set; }
+    [field: SerializeField] public Color Color { get; private set; }
+    [field: SerializeField] public Material Material { get; private set; }
+    [field: SerializeField] public Sprite Sprite_Background { get; private set; }
+    [field: SerializeField] public float MinBaseStatMulti { get; private set; }
+    [field: SerializeField] public float MaxBaseStatMulti { get; private set; }
+}
