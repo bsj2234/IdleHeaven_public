ebaa510c620b1b16175259cc95f53f732b170738
변석진 <bsj2234@naver.com>
2024-07-31 18:31:26 +0900

    [update] 세로 화면비에따라 속도 조절

--- Assets/IdleHeaven/Scripts/VFX/Transflator.cs
+++ Assets/IdleHeaven/Scripts/VFX/Transflator.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Transflator : MonoBehaviour
 {
     [SerializeField] Vector3 Accel;
-
+    [SerializeField] bool Ratio;
+    [SerializeField] float original;
+    [SerializeField] float dest;
+    float ratio = 1f;
     private void OnEnable()
     {
         transform.localPosition= Vector3.zero;
+        dest = Screen.height;
+        if(Ratio)
+        {
+            ratio = dest / original;
+        }
     }
     void Update()
     {
-        transform.localPosition = transform.localPosition + Accel * Time.deltaTime;
+        transform.localPosition = transform.localPosition + Accel * ratio * Time.deltaTime;
     }
 }
